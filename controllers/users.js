5312fbcfbbed3decef06a7c16c59b893c9ab18b1
Chris Roman <Christopherroman85@gmail.com>
2018-10-16 21:24:39 -0500

    created new user ejs and post route

--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,14 @@ router.get('/new', (req, res) => {
 	res.render('users/new.ejs')
 });
 
-
+router.post('/', (req, res) => {
+	User.create(req.body, (err, createdUser) => {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect('/users')
+		}
+	})
+})
 
 module.exports = router
