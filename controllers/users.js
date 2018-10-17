759de4b422505386aa3c8f7635e228866c271498
Chris Roman <Christopherroman85@gmail.com>
2018-10-16 21:45:11 -0500

    created delete route and started put route

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/users');
 const Image = require('../models/images');
 
-
+// User Index Route
 router.get('/', (req, res) => {
 	User.find({}, (err, foundUsers) => {
 		res.render('users/index.ejs', {
@@ -12,10 +12,21 @@ router.get('/', (req, res) => {
 	});
 });
 
+// New User route
 router.get('/new', (req, res) => {
 	res.render('users/new.ejs')
 });
 
+// User Show route
+router.get('/:index', (req, res) => {
+	User.findById(req.params.index, (err, foundUser) => {
+		res.render('users/show.ejs', {
+			user: foundUser
+		});
+	});
+});
+
+// User Post route
 router.post('/', (req, res) => {
 	User.create(req.body, (err, createdUser) => {
 		if(err) {
@@ -25,5 +36,27 @@ router.post('/', (req, res) => {
 		}
 	})
 })
+
+// User Put route
+router.put('/:index', (req, res) => {
+	User.findByIdAndUpdate(req.params.index)
+})
+
+
+// User Delete route
+router.delete('/:index', (req, res) => {
+	User.findByIdAndDelete(req.params.index, (err, deletedUser) => {
+		if(err){
+			console.log(err);
+		} else {
+		res.redirect('/users')
+		}
+	})
+})
+
+
+
+
+
 
 module.exports = router
