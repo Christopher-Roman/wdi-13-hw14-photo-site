6e834eeee9c92f94c2700cec827dbafcad8a7b2c
Chris Roman <Christopherroman85@gmail.com>
2018-10-16 22:18:53 -0500

    image index route works

--- controllers/images.js
+++ controllers/images.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const Image = require('../models/images');
 const User = require('../models/users');
 
-
+router.get('/', (req, res) => {
+	Image.find({}, (err, foundImages) => {
+		res.render('images/index.ejs', {
+			images: foundImages
+		});
+	});
+});
 
 
 module.exports = router
