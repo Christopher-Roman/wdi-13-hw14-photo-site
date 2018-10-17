92c1266b7963fa22fcb4d2e96fb9fd50d5a9133d
Chris Roman <Christopherroman85@gmail.com>
2018-10-16 21:03:53 -0500

    connected routes and set up index page

---
+++ controllers/images.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const Image = require('../models/images');
+const User = require('../models/users');
+
+
+
+
+module.exports = router
---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users');
+const Image = require('../models/images');
+
+
+
+
+module.exports = router
